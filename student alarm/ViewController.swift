fc1cb45a5e85e5ca3f1b4846369cfaa248592b15
Tina Chen <yunchu101@gmail.com>
2020-07-07 19:32:09 +0800

    Initial Commit

---
+++ student alarm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  student alarm
+//
+//  Created by Mac on 2020/7/7.
+//  Copyright © 2020 studentalarm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
